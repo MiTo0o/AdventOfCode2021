049c8d838f1186a132bb4efde76b341c018faa84
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-12-07 20:55:11 -0800

    wart

--- day5.py
+++ day5.py
@@ -7,8 +7,8 @@ with open('input/d5.txt') as f:
     parsed.append([(int(t[0]), int(t[1])), (int(t2[0]), int(t2[1]))])
   # cover = 0
   matrix = []
-  for i in range(10):
-    matrix.append([0] * 10)
+  for i in range(1000):
+    matrix.append([0] * 1000)
   for coord in parsed:
     x1 = coord[0][0]
     y1 = coord[0][1]
@@ -24,23 +24,39 @@ with open('input/d5.txt') as f:
         else:
           for i in range(y2, y1 + 1):
             matrix[i][x1] += 1
-      if y2 == y1:
+      elif y2 == y1:
         if x1 > x2:
           for i in range(x2, x1 + 1):
             matrix[y1][i] += 1
         else:
           for i in range(x1, x2 + 1):
             matrix[y1][i] += 1
-    
+      elif x1 > x2 and y1 > y2:
+        matrix[y1][x1] += 1
+        while x1 != x2:
+          x1 -= 1
+          y1 -= 1
+          matrix[y1][x1] += 1
+      elif x1 < x2 and y1 < y2:
+        matrix[y1][x1] += 1
+        while x1 != x2:
+          x1 += 1
+          y1 += 1
+          matrix[y1][x1] += 1
+      elif x1 > x2 and y1 < y2:
+        matrix[y1][x1] += 1
+        while x1 != x2:
+          x1 -= 1
+          y1 += 1
+          matrix[y1][x1] += 1
+      elif x1 < x2 and y1 > y2:
+        matrix[y1][x1] += 1
+        while x1 != x2:
+          x1 += 1
+          y1 -= 1
+          matrix[y1][x1] += 1
+          
 count = 0
-# print(parsed)
 for i in matrix:
-  print(i)
-for i in matrix:
-  count += i.count(2)
+  count += len(list(filter(lambda x: x > 1, i)))
 print(count)
-
-
-
-# def overlap(c1, c2):
-#   pass
