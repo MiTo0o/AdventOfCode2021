e75b63f3f83a7f49c391bec5eddd8d25f845ddb5
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-12-09 01:50:09 -0800

    greatly improved day9

--- day9.py
+++ day9.py
@@ -1,98 +1,36 @@
-
 with open('input/d9.txt') as f:
-  data = [list(line.strip()) for line in f]
-
-def helper(grid, i, j, temp):
-  if grid[i][j] == "9":
-      return
-  grid[i][j] = "9"
-  temp[0] += 1
-  if i + 1 < len(grid):
-      helper(grid, i + 1, j, temp)
-  if i - 1 >= 0:
-      helper(grid, i - 1, j, temp)
-  if j + 1 < len(grid[i]):
-      helper(grid, i, j + 1, temp)
-  if j - 1 >= 0:
-      helper(grid, i, j - 1, temp)
-
-s = []
-for i in range(len(data)):
-  for j in range(len(data[i])):
-    if data[i][j] != "9":
-      temp = [0]
-      helper(data, i, j, temp)
-      s.append(temp[0])
-s.sort()
-print(s[-1] * s[-2] * s[-3])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  cave = [list(line.strip()) for line in f]
 # part 1
+risk_level = 0
+for i in range(len(cave)):
+  for j in range(len(cave[i])):
+    adjacent_coords = [[i, j + 1], [i, j - 1], [i - 1, j], [i + 1, j]]
+    possible_coords = [k for k in adjacent_coords if k[0] < len(cave) and k[1] < len(cave[i]) and k[0] >= 0 and k[1] >= 0]
+    if all([cave[k[0]][k[1]] > cave[i][j] for k in possible_coords]):
+      risk_level += int(cave[i][j]) + 1
+print(risk_level)
 
-# with open('input/d9.txt') as f:
-#   data = [line.strip() for line in f]
-#   s = 0
-#   print(data)
-#   for i in range(len(data)):
-#     # print(data[i])
-#     for j in range(len(data[i])):
-#       combo = [[i, j + 1], [i, j - 1], [i - 1, j], [i + 1, j]]
-#       possible = [k for k in combo if k[0] < len(data) and k[1] < len(data[i]) and k[0] > -1 and k[1] > -1]
-#       flag = True
-#       for k in possible:
+# Part 2 (modifies cave array)
+def helper(cave, i, j, basin_size):
+  if cave[i][j] == "9":
+    return
+  cave[i][j] = "9"
+  basin_size[0] += 1
+  if i + 1 < len(cave):
+    helper(cave, i + 1, j, basin_size)
+  if i - 1 >= 0:
+    helper(cave, i - 1, j, basin_size)
+  if j + 1 < len(cave[i]):
+    helper(cave, i, j + 1, basin_size)
+  if j - 1 >= 0:
+    helper(cave, i, j - 1, basin_size)
 
-#         if data[k[0]][k[1]] <= data[i][j]:
-#           flag = False
-#       if flag:
-#         print(i, j)
-#         print(data[i][j])
-#         s += int(data[i][j]) + 1
-
-#     # for j in range(len(i)):
-#     #   if j == len(i) - 1:
-#     #     if int(i[j]) < int(i[j - 1]):
-#     #       s += int(i[j]) + 1
-#     #   elif j == 0:
-#     #     if int(i[j]) < int(i[j + 1]):
-#     #       s += int(i[j]) + 1
-#     #   else:
-#     #     if int(i[j]) < int(i[j + 1]) and int(i[j - 1]):
-#     #       s += int(i[j]) + 1
-#   print(s)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+all_basins = []
+for i in range(len(cave)):
+  for j in range(len(cave[i])):
+    if cave[i][j] != "9":
+      basin_size = [0]
+      helper(cave, i, j, basin_size)
+      all_basins.append(basin_size[0])
+all_basins.sort()
+print(all_basins[-1] * all_basins[-2] * all_basins[-3])
