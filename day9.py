03acc7607df5a89b16fb60faf495a8a3a6d204ed
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-12-09 01:53:39 -0800

    day9 spacing

--- day9.py
+++ day9.py
@@ -1,11 +1,13 @@
 with open('input/d9.txt') as f:
   cave = [list(line.strip()) for line in f]
+  
 # part 1
 risk_level = 0
 for i in range(len(cave)):
   for j in range(len(cave[i])):
     adjacent_coords = [[i, j + 1], [i, j - 1], [i - 1, j], [i + 1, j]]
     possible_coords = [k for k in adjacent_coords if k[0] < len(cave) and k[1] < len(cave[i]) and k[0] >= 0 and k[1] >= 0]
+    
     if all([cave[k[0]][k[1]] > cave[i][j] for k in possible_coords]):
       risk_level += int(cave[i][j]) + 1
 print(risk_level)
@@ -32,5 +34,6 @@ for i in range(len(cave)):
       basin_size = [0]
       helper(cave, i, j, basin_size)
       all_basins.append(basin_size[0])
+      
 all_basins.sort()
 print(all_basins[-1] * all_basins[-2] * all_basins[-3])
